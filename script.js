56a6eba7352105f374550ca9d31b73141756157d
Nicolas <nicolas_rodrigues_27@outlook.com>
2023-01-21 22:16:52 -0300

    avançando no Js

--- script.js
+++ script.js
@@ -1,13 +1,29 @@
-const form = document.querySelector('form')
-const nlwSetup = new NLWSetup(form)
+const form = document.querySelector("#form-habits");
+const nlwSetup = new NLWSetup(form);
 
-const data = { 
-    exercise: ['01-01', '01-02', '01-06'], 
-    water: ['01-01', '01-04', '01-05'],
-    diet: ['01-01', '01-03', '01-06'],
-    read: ['01-01', '01-03', '01-04', '01-06'],
-    meditate: ['01-02', '01-03', '01-06', '01-09'],
+const button = document.querySelector("header button");
+
+button.addEventListener("click", add);
+form.addEventListener("change", save);
+
+function add() {
+  const today = new Date().toLocaleDateString("pt-br").slice(0, -5);
+  //const today = "01/01" dá pra usar essa linha de código pra testar com datas diferentes, pra ter certeza que tá funcionando mesmo
+  const dayExists = nlwSetup.dayExists(today);
+
+  if (dayExists) {
+    alert("Impossível, dia já incluso ❌");
+    return;
   }
-  
-  nlwSetup.setData(data)
-  nlwSetup.load()   
+
+  alert("Deu certo, dia adicionado ✔️");
+  nlwSetup.addDay(today);
+}
+
+function save() {
+  localStorage.setItem("NLWSetup-habits", JSON.stringify(nlwSetup.data));
+}
+
+const data = JSON.parse(localStorage.getItem("NLWSetup-habits")) || {};
+nlwSetup.setData(data);
+nlwSetup.load();
